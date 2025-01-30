e86304506b803fe0dd29eb318da9dd24ebb02e25
raulvt02 <vera.trashorras.raul@iescamas.es>
2025-01-30 10:12:10 +0100

    Completada la instancia distancia

--- code/src/main/java/com/endes/figuras/Punto.java
+++ code/src/main/java/com/endes/figuras/Punto.java
@@ -12,4 +12,15 @@ public class Punto{
 	public double getY () {
 		return y;
 	}
+	public void setX (double x) {
+		this.x =x;
+	}
+	public void setY(double y) {
+		this.y =y;
+	}
+	public double distancia (Punto otro) {
+		double diferenciaX = this.x - otro.getX();
+        double diferenciaY = this.y - otro.getY();
+        return Math.sqrt(Math.pow(diferenciaX, 2) + Math.pow(diferenciaY, 2));
+	}
 }

f7f2c4e85fbe8229dc3dc87062d939a916c24598
raulvt02 <vera.trashorras.raul@iescamas.es>
2025-01-30 10:02:55 +0100

    He creado constructor punto, falta calcular distancia

---
+++ code/src/main/java/com/endes/figuras/Punto.java
@@ -0,0 +1,15 @@
+package com.endes.figuras;
+
+public class Punto{
+	private double x, y;
+	public Punto (double x, double y) {
+		this.x = x;
+		this.y = y;
+	}
+	public double getX () {
+		return x;
+	}
+	public double getY () {
+		return y;
+	}
+}

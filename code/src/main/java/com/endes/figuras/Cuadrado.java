c2038b2e2bec617f80ee3d4cd7c1e6670bb8a024
Sergio <sergio.garciapedrero7@gmail.com>
2025-01-31 11:16:01 +0100

    Finalizacion de la clase cuadrado

--- code/src/main/java/com/endes/figuras/Cuadrado.java
+++ code/src/main/java/com/endes/figuras/Cuadrado.java
@@ -22,7 +22,20 @@ public class Cuadrado extends FiguraGeometrica{
 	}
 	
 	public void rotar(double angulo) {
-		
+		double radianes = Math.toRadians(angulo);
+        double cosA = Math.cos(radianes);
+        double sinA = Math.sin(radianes);
+        
+        double centroX = esquina.getX() + lado / 2;
+        double centroY = esquina.getY() + lado / 2;
+        
+        double x = esquina.getX() - centroX;
+        double y = esquina.getY() - centroY;
+        
+        double nuevoX = x * cosA - y * sinA + centroX;
+        double nuevoY = x * sinA + y * cosA + centroY;
+        
+        esquina = new Punto(nuevoX, nuevoY);
 	}
 	
 	

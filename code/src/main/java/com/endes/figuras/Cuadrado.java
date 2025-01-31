9e12eb93b48f7b4e481291dd8dc336f905f6594a
Sergio <sergio.garciapedrero7@gmail.com>
2025-01-31 11:05:20 +0100

    Corrección de errores que habian en la clase

--- code/src/main/java/com/endes/figuras/Cuadrado.java
+++ code/src/main/java/com/endes/figuras/Cuadrado.java
@@ -2,22 +2,23 @@ package com.endes.figuras;
 
 public class Cuadrado extends FiguraGeometrica{
 	double lado;
+	Punto esquina;
 	public Cuadrado(String nombre, String color, Punto esquina, double lado) {
 		super(nombre, color);
-		this.lado=lado;
-		// TODO Auto-generated constructor stub
+		this.esquina = esquina;
+		this.lado = lado;
 	}
 
 	@Override
 	public double area() {
-		// TODO Auto-generated method stub
-		return 0;
+		double area = lado * lado;
+		return area;
 	}
 
 	@Override
 	public double perimetro() {
-		// TODO Auto-generated method stub
-		return 0;
+		double perimetro = lado * 4;
+		return perimetro;
 	}
 	
 	public void rotar(double angulo) {

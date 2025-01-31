6258a8e9ce300e7e175de46a9881f4e7aa91ca47
periquito18 <mate.palacios.sergio@iescamas.es>
2025-01-31 11:10:26 +0100

    Creada Clase Circulo

--- code/src/main/java/com/endes/figuras/Circulo.java
+++ code/src/main/java/com/endes/figuras/Circulo.java
@@ -1,21 +1,31 @@
 package com.endes.figuras;
 
 public class Circulo extends FiguraGeometrica{
-	private double radio;
+	double radio;
+	Punto centro;
+	public Circulo(String nombre, String color, Punto centro, double radio) {
+		super(nombre, color);
+		this.radio = radio;
+	}
 	
-
-/**
 	@Override
 	public double area() {
-		// TODO Auto-generated method stub
-		return 0;
+		return Math.PI * radio * radio;
 	}
-
+	
 	@Override
 	public double perimetro() {
-		// TODO Auto-generated method stub
-		return 0;
+		return Math.PI * radio * 2;
 	}
-**/
-
-}
+	
+	@Override
+	public void rotar(double angulo) {
+		System.out.println("Rotando el circulo " + angulo + " grados.");
+	}
+	
+	@Override
+	public void trasladar(double dx, double dy) {
+		System.out.println("Trasladando el circulo en (" + dx + ", " + dy + ").");
+	}
+	
+ }
